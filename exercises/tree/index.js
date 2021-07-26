38dca20b2c9135b2d9c0acb501a434bfd52750c9
derekscode <17056283+derekscode@users.noreply.github.com>
2021-07-25 17:34:57 -0700

    update

--- exercises/tree/index.js
+++ exercises/tree/index.js
@@ -11,9 +11,45 @@
 // function that gets called with each element in the tree
 
 class Node {
-	// test
+	constructor(data) {
+		this.data = data;
+		this.children = [];
+	}
+
+	add(data) {
+		this.children.push(new Node(data));
+	}
+
+	remove(data) {
+		this.children = this.children.filter((node) => {
+			return node.data !== data;
+		});
+	}
 }
 
-class Tree {}
+class Tree {
+	constructor() {
+		this.root = null;
+	}
+	traverseBF(fn) {
+		let arr = [this.root];
+
+		while (arr.length) {
+			const node = arr.shift();
+			arr.push(...node.children);
+			fn(node);
+		}
+	}
+
+	traverseDF(fn) {
+		let arr = [this.root];
+
+		while (arr.length) {
+			const node = arr.shift();
+			arr.unshift(...node.children);
+			fn(node);
+		}
+	}
+}
 
 module.exports = { Tree, Node };
